a64c438fbc77b21a2a2a3bc1411a54701098e317
MarKA <lwermarce@gmail.com>
2025-05-07 18:43:40 -0600

    prueba

---
+++ build/precache-manifest.37e1083c3e171395d71fd4ae186e787c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee0a7ed972e37b85fb0",
+    "url": "/static/css/main.172289cd.chunk.css"
+  },
+  {
+    "revision": "fee0a7ed972e37b85fb0",
+    "url": "/static/js/main.fee0a7ed.chunk.js"
+  },
+  {
+    "revision": "d015ae50e45e379cfbab",
+    "url": "/static/css/1.861c550a.chunk.css"
+  },
+  {
+    "revision": "d015ae50e45e379cfbab",
+    "url": "/static/js/1.d015ae50.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0c825aaff277697bb85a38fa0c0f982",
+    "url": "/index.html"
+  }
+];
